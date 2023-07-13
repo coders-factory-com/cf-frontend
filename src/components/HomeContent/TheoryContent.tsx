e0399354536f84c513098107b1af30723135aa0d
Kamil Dróżdż <kamil12x@wp.pl>
2023-07-13 15:19:39 +0200

    improvement slightly, scrollbar as well as adding icons as in figma, improving styling

--- src/components/HomeContent/TheoryContent.tsx
+++ src/components/HomeContent/TheoryContent.tsx
@@ -31,8 +31,8 @@ function TheoryContent() {
 					</div>
 				</div>
 			</div>
-			<div className=' left-[81px] ml-[10%] top-[30px]  flex-col justify-start items-start flex'>
-				<div className='self-stretch  py-[42px] bg-white border  border-neutral-200 flex-col justify-start items-start gap-6 flex'>
+			<div className='theoryContent left-[81px] ml-[10%] top-[30px]  flex-col justify-start items-start flex'>
+				<div className='self-stretch  py-[42px] bg-white border mb-48  border-neutral-200 flex-col justify-start items-start gap-6 flex'>
 					<div className='self-stretch px-6 flex-col justify-start items-start gap-10 flex'>
 						<Section title='Introduction' timeRead='5'>
 							Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia
--- src/components/TopicCard.tsx
+++ src/components/TopicCard.tsx
@@ -1,10 +1,11 @@
 import { TopicCardProps } from '@/typings/typings';
+import Image from 'next/image';
 
 function TopicCard({ title, isActive, onClick }: TopicCardProps & { isActive: boolean; onClick: () => void }) {
 	return (
 		<div onClick={onClick} className={`px-4 py-4 bg-white border-b-[1px] cursor-pointer justify-start items-start gap-2 flex ${isActive ? 'border-blue-600' : 'border-neutral-200'}`}>
 			<div className='w-[46px] justify-start items-start flex'>
-				<div className='text-neutral-900 text-[14px] font-semibold leading-none tracking-wide'>{title}</div>
+				<div className='text-neutral-900 text-[14px] font-semibold leading-none tracking-wide'>{title}iI</div>
 			</div>
 		</div>
 	);
