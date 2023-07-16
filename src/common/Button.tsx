1b05ef19d3f3267f8305b1196d95ff9983c8806d
Kamil Drozdz <kamil12x@wp.pl>
2023-07-16 16:03:02 +0200

    adding JIT mode to talwind which instantly updates changes as well as clsx library for merging classes, fixing some things from main component and adding prettier to talwind

--- src/common/Button.tsx
+++ src/common/Button.tsx
@@ -1,6 +1,8 @@
+import clsx from 'clsx';
+
 const Button = ({ type, text, onClick, disabled, addedClassName = '' }: ButtonProps) => {
 	return (
-		<button type={type} disabled={disabled} className={`${addedClassName} btn-primary`} onClick={onClick}>
+		<button type={type} disabled={disabled} className={clsx(`disabled:opacity-60 my-4 hover:bg-white border px-6 py-4 text-center cursor-pointer hover:text-blue-600 text-[14px] font-normal bg-blue-600 text-white transition-colors duration-300 ease-in-out leading-none tracking-wide`, addedClassName)} onClick={onClick}>
 			{text}
 		</button>
 	);
