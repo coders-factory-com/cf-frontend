69026d0f0b27c904c6f1564372e39d4eddbe8e53
Kamil Drozdz <kamil12x@wp.pl>
2023-07-17 06:44:32 +0200

    adding a field where you can see the password on request as well as dividing components into smaller reusable parts, adding a few smaller components that were reused

---
+++ src/common/Input.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import { FiEye, FiEyeOff } from 'react-icons/fi';
+
+export const Input = ({ icon, placeholder, isPassword = false, ...props }: InputProps) => {
+	const [showPassword, setShowPassword] = useState(false);
+
+	return (
+		<div className='relative'>
+			{icon && <div className='absolute text-2xl  top-1/2 left-4 transform -translate-y-1/2'>{icon}</div>}
+			{isPassword && (
+				<div className='absolute text-2xl top-1/2 right-4 transform -translate-y-1/2 cursor-pointer' onClick={() => setShowPassword(prev => !prev)}>
+					{showPassword ? <FiEyeOff /> : <FiEye />}
+				</div>
+			)}
+			<input type={isPassword && !showPassword ? 'password' : 'text'} placeholder={placeholder} className={`appearance-none py-4 w-full border-[1px] border-gray-200 rounded-lg hover:border-black ${icon && 'pl-12'}`} {...props} />
+		</div>
+	);
+};
--- src/components/ForgotPasswordForm.tsx
+++ src/components/ForgotPasswordForm.tsx
@@ -2,6 +2,8 @@
 import Link from 'next/link';
 import { FiUser } from 'react-icons/fi';
 import { FormEvent, useState } from 'react';
+import { Input } from '@/common/Input';
+import FormHeader from './FormHeader';
 
 function ForgotPasswordForm() {
 	const [email, setEmail] = useState('');
@@ -12,24 +14,14 @@ function ForgotPasswordForm() {
 
 	return (
 		<div className='w-[500px]'>
-			<h2 className='flex my-6 justify-center items-center'>
-				<div className='p-0.5 bg-gray-300 rounded justify-center items-center gap-2.5 flex mr-4'>
-					<div className='text-black text-[14px] font-semibold leading-none tracking-wide'>&lt;/&gt;</div>
-				</div>
-				<div className='text-black text-[14px] font-bold leading-none whitespace-nowrap tracking-wide'>Coders-Factory</div>
-			</h2>
-			<h3 className='text-center font-bold text-2xl'>For Developers</h3>
-			<p className='!my-4 w-full text-center'>Practice coding, prepare for interviews, and get hired.</p>
+			<FormHeader />
 			<div className='shadow-lg pt-4 px-6 bg-white'>
 				<div className='mt-6 w-full text-center'>
 					<h3 className='font-bold text-xl '> Forgot Password</h3>
 					<p className='text-gray-500'>No Problem! Enter your email or username below and we will send you an email with instruction to reset your</p>
 				</div>
 				<form className='relative z-[3] mt-4 py-6 space-y-8' onSubmit={handleSubmit}>
-					<div className='relative'>
-						<FiUser className='absolute text-2xl  top-1/2 left-4 transform -translate-y-1/2' />
-						<input type='email' placeholder='E-mail or Username' value={email} className='appearance-none py-4 w-full border-[1px] border-gray-200 rounded-lg hover:border-black pl-12' onChange={e => setEmail(e.target.value)} />
-					</div>
+					<Input placeholder='E-mail or Username' value={email} onChange={e => setEmail(e.target.value)} />
 					<button className=' bg-[#00751f] hover:bg-[#0f6125] transition-colors duration-200 ease-in-out py-3 px-6 w-full text-white font-bold rounded-lg' type='submit'>
 						Send reset link
 					</button>
