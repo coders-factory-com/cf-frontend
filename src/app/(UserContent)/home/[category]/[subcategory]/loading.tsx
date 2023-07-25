8f924b2df5a0650e1d4a52cb4a5d4b7f8c1b8b72
Kamil Drozdz <kamil12x@wp.pl>
2023-07-25 03:43:09 +0200

    adding loaders to individual paths - for some reason it doesn't work from the main folder

---
+++ src/app/(UserContent)/home/[category]/[subcategory]/loading.tsx
@@ -0,0 +1,11 @@
+import Loader from '@/common/Loader';
+
+function loading() {
+	return (
+		<div className='w-[calc(100%-191px)] left-[191px] relative h-screen flex flex-col justify-center items-center'>
+			<Loader size='12' width='fit' />
+		</div>
+	);
+}
+
+export default loading;
