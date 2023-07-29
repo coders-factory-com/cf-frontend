6c526f660384b70113e9ea699a17dc093d5608c9
Szczepan M <szczypiorpan123@gmail.com>
2023-07-29 14:25:03 +0200

    Add Navigation enum for routing

---
+++ src/enums/navigation.enum.ts
@@ -0,0 +1,7 @@
+export enum Navigation {
+    Main = '/',
+    Home = '/home',
+    AuthLogin = '/auth/login',
+    AuthSignUp = '/auth/signup',
+    AuthForgotPassword = '/auth/forgot-password',
+}
