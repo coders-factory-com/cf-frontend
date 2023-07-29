74df7e101073f11c749650d29dc682274e383133
Szczepan M <szczypiorpan123@gmail.com>
2023-07-29 14:59:41 +0200

    Refactor file structure and fix typo in page.tsx

--- src/app/auth/forgot_password/page.tsx
+++ src/app/auth/forgot_password/page.tsx
@@ -2,7 +2,7 @@ import ForgotPasswordForm from '@/components/forms/ForgotPasswordForm';
 
 function ForgotPasswordPage() {
 	return (
-		<div className=' h-screen min-h-full relative p-8 bg-neutral-50 flex justify-start flex-col items-center'>
+		<div className='h-screen min-h-full relative p-8 bg-neutral-50 flex justify-start flex-col items-center'>
 			<ForgotPasswordForm />
 		</div>
 	);
